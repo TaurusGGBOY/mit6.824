e83e95ca852d4b00acae687a483d6ce28ba1505b
ggbubuntu <854357635@qq.com>
2021-08-17 08:20:35 -0700

    almost finish 2D

--- assignment1/6.824/src/main/testGo.go
+++ assignment1/6.824/src/main/testGo.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
+import "time"
 
 func main() {
-	a := make([]int, 0)
-	a = append(a, 0)
-	a = append(a, 1)
-	a = append(a, 2)
-	a = append(a, 3)
-	a = append(a, 4)
-	fmt.Println("hello %v", a[4:])
-	fmt.Println("hello2 %v", a[5:])
-	fmt.Println("hello2 %v", a[6:])
+	go func() {
+		for{
+			print("test\n")
+		}
+	}()
+	time.Sleep(time.Duration(10)*time.Millisecond)
 }
